5f258937ad03613d32ba6fd56b6753604c506318
3lsy <echavez-@student.42.fr>
2024-07-17 11:37:12 +0200

    setup

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+sudo apt-get update
+# Install Docker dependencies
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add Docker APT repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package list again
+sudo apt-get update
+
+# Install Docker
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Add user to Docker group
+sudo usermod -aG docker vagrant
+newgrp docker
+
+# Enable Docker service
+sudo systemctl enable docker
+sudo systemctl start docker
+echo "127.0.0.1	echavez-.42.fr" | sudo tee -a /etc/hosts
+
+mkdir -p /home/echavez-/data
