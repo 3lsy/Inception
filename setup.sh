dabe898b257ca3f68f1ce8d94e73801a94b80925
3lsy <echavez-@student.42.fr>
2024-07-18 12:14:40 +0200

    y def docker install

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ echo \
 sudo apt-get update
 
 # Install Docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Add user to Docker group
 sudo usermod -aG docker echavez-
